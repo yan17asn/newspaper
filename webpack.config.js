82b961fb6487a14b6c422932be2e9d6bd7dbc3ba
Rui <Wei.Y.Rui2@seattlecolleges.edu>
2020-06-13 19:11:44 +0000

    All my files are belong to us

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'app': './js/index.js',
+        'styles': './sass/styles.sass'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i, // .scss or .sass
+                loader: ExtractTextWebpackPlugin.extract([
+                    'css-loader',
+                    'sass-loader'
+                ])
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin({
+            filename: '[name].css',
+            allChunks: true
+        })
+    ],
+    devServer: {
+        contentBase: __dirname,
+        publicPath: '/dist/',
+        port: 8080,
+        host: '0.0.0.0',
+        hot: true,
+        disableHostCheck: true
+    }
+}
